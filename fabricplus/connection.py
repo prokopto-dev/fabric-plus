241ac82cbf0a5eb2da5b033516017953f89189dc
Courtney Caldwell <courtneyccaldwell@gmail.com>
2024-05-17 15:52:33 -0400

    Added connect to jumphost function into Connection to allow for kwargs to be passed in
    Signed-off-by: Courtney Caldwell <courtneyccaldwell@gmail.com>

--- fabricplus/connection.py
+++ fabricplus/connection.py
@@ -1,15 +1,14 @@
 from fabric.connection import Connection, opens
 import fabric.transfer
 from fabricplus.transfer import TransferPlus
-from paramiko import WarningPolicy, SSHClient, Transport, PKey
-from socket import socket
-from fabricplus.paramiko_modifications.client import SSHJumpClient
+from paramiko import WarningPolicy, SSHClient, Transport
+from fabricplus.paramiko_modifications.client import SSHJumpClient, simple_auth_handler
 from scp import SCPClient
 
-from typing import Optional, Union
+from typing import Optional, Union, Callable, List, Any
 
 class ConnectionPlus(Connection):
-    def __init__(self, *args, jumphost_target: Optional[Union[SSHJumpClient, SSHClient, str, Connection]] = None, scp: bool = False, **kwargs):
+    def __init__(self, *args, jumphost_target: Optional[Union[SSHJumpClient, SSHClient, str, Connection]] = None, scp: bool = False, jump_uname: Optional[str] = None, **kwargs):
         """
         Args:
         
@@ -17,30 +16,101 @@ class ConnectionPlus(Connection):
         super().__init__(*args, **kwargs)
         self._scp: Optional[SCPClient] = None
         self.__scp: bool = scp
-        self.client = self.__setup_jumphost(jumphost_target=jumphost_target)
+        self.client = self.__setup_jumphost(jumphost_target=jumphost_target, username=jump_uname)
         self.client.set_missing_host_key_policy(WarningPolicy())
         self.client.load_system_host_keys()
         
+    def __connect_client(self,
+                         client: Union[SSHJumpClient, SSHClient],
+                         username: Optional[str] = None) -> None:
+        # Short-circuit
+        if self.is_connected:
+            return
+        err = "Refusing to be ambiguous: connect() kwarg '{}' was given both via regular arg and via connect_kwargs!"  # noqa
+        # These may not be given, period
+        for key in """
+            hostname
+            port
+            username
+        """.split():
+            if key in self.connect_kwargs:
+                raise ValueError(err.format(key))
+        # These may be given one way or the other, but not both
+        if (
+            "timeout" in self.connect_kwargs
+            and self.connect_timeout is not None
+        ):
+            raise ValueError(err.format("timeout"))
+        # No conflicts -> merge 'em together
+        kwargs = dict(
+            self.connect_kwargs,
+            username=self.user,
+            hostname=self.host,
+            port=self.port,
+        )
+        if self.gateway:
+            kwargs["sock"] = self.open_gateway()
+        if self.connect_timeout:
+            kwargs["timeout"] = self.connect_timeout
+        # Strip out empty defaults for less noisy debugging
+        if "key_filename" in kwargs and not kwargs["key_filename"]:
+            del kwargs["key_filename"]
+        auth_strategy_class = self.authentication.strategy_class
+        if auth_strategy_class is not None:
+            # Pop connect_kwargs related to auth to avoid giving Paramiko
+            # conflicting signals.
+            for key in (
+                "allow_agent",
+                "key_filename",
+                "look_for_keys",
+                "passphrase",
+                "password",
+                "pkey",
+                "username",
+            ):
+                kwargs.pop(key, None)
+
+            kwargs["auth_strategy"] = auth_strategy_class(
+                ssh_config=self.ssh_config,
+                fabric_config=self.config,
+                username=username or self.user,
+            )
+        client.connect(**kwargs)
+        return None
+        
     
-    def __setup_jumphost(self, *args, jumphost_target: Optional[Union[SSHJumpClient, SSHClient, str, Connection]], **kwargs) -> SSHClient:
+    def __setup_jumphost(self,
+                         *args,
+                         jumphost_target: Optional[Union[SSHJumpClient, SSHClient, str, Connection]],
+                         interactive_prompt: bool = False,
+                         jump_uname: Optional[str] = None,
+                         **kwargs) -> SSHClient:
         _client: Optional[Union[SSHJumpClient, SSHClient]] = None
+        _auth_handler: Optional[Callable[..., List[Any]]] = simple_auth_handler if interactive_prompt else None
         if jumphost_target is not None:
             if isinstance(jumphost_target, SSHJumpClient):
                 _client = jumphost_target
             elif isinstance(jumphost_target, SSHClient):
                 _client = jumphost_target
             elif isinstance(jumphost_target, str):
-                _client = SSHJumpClient()
+                _client = SSHJumpClient(auth_handler=_auth_handler)
                 _client.set_missing_host_key_policy(WarningPolicy())
                 _client.load_system_host_keys()
-                _client.connect(hostname=jumphost_target, **kwargs)
+                self.__connect_client(_client)
             elif isinstance(jumphost_target, Connection):
                 if jumphost_target.client is None:
                     jumphost_target.open()
                 _client = jumphost_target.client
             else:
                 raise TypeError("The jumphost_target must be an instance of SSHJumpClient, SSHClient, str, or Connection.")
-        return SSHJumpClient(*args, jump_session=_client, **kwargs)
+        return SSHJumpClient(*args, jump_session=_client, auth_handler=_auth_handler, **kwargs)
+    
+    @property
+    def jump_client(self) -> Optional[SSHJumpClient]:
+        try:
+            return self.client._jump_session # type: ignore
+        except AttributeError:
+            return None
     
     @opens
     def scp(self):
