6864fa320136935da6c0e5875a070d7cd7b25c54
Courtney Caldwell <courtneyccaldwell@gmail.com>
2024-05-17 12:46:08 -0400

    Imported, with license information, the jumpclient files
    Signed-off-by: Courtney Caldwell <courtneyccaldwell@gmail.com>

--- fabricplus/connection.py
+++ fabricplus/connection.py
@@ -1,6 +1,22 @@
-from fabric.connection import Connection
+from fabric.connection import Connection, opens
 from fabricplus.transfer import TransferPlus
+from fabricplus.paramiko_modifications.client import SSHJumpClient
+from scp import SCPClient
+
+from typing import Optional
 
 class ConnectionPlus(Connection):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._scp: Optional[SCPClient] = None
+        self.client = SSHJumpClient()
+    
+    @opens
+    def scp(self):
+        if self._scp is None:
+            try:
+                # Ignoring type because we're handling the exception in cases of Nones.
+                self._scp = SCPClient(self.client.get_transport()) # type: ignore
+            except AttributeError:
+                raise AttributeError("The base fabric Connection object does not have a client initialized.")
+        return self._scp
